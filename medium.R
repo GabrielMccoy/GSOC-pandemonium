941e5131b0a125ebd38ed450f0420b1752ccd6a8
GabrielMccoy <gmcc0022@student.monash.edu>
2025-03-07 12:00:34 +1100

    Update medium.R

--- medium.R
+++ medium.R
@@ -13,7 +13,7 @@ server <- function(input, output) {
                                                                                         axes = TRUE, scale_factor = 0.4), quoted = FALSE)
   output$detour2 <- detourr::shinyRenderDisplayScatter2d(detourr::detour(shared_detour, 
                                                                                 detourr::tour_aes(projection = c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"),colour = Species)) |>
-                                                                  detourr::tour_path(detourr::grand_tour(2), fps = 60, 
+                                                                  detourr::tour_path(detourr::local_tour(tourr::basis_random(n=4)), fps = 60, 
                                                                                      max_bases=20) |>
                                                                   detourr::show_scatter(alpha = 0.7, 
                                                                                         axes = TRUE, scale_factor = 0.4), quoted = FALSE)
